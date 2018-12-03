218c00abf92b36e4043733ec2e424154faa1e893
Eliakin Costa <eliakim170@gmail.com>
2018-12-03 00:26:25 -0300

    07: Basic Web Handling With Views

---
+++ views/tutorial/tests.py
@@ -0,0 +1,45 @@
+import unittest
+
+from pyramid import testing
+
+
+class TutorialViewTests(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_home(self):
+        from .views import home
+
+        request = testing.DummyRequest()
+        response = home(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Visit', response.body)
+
+    def test_hello(self):
+        from .views import hello
+
+        request = testing.DummyRequest()
+        response = hello(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Go back', response.body)
+
+
+class TutorialFunctionalTests(unittest.TestCase):
+
+    def setUp(self):
+        from tutorial import  main
+        app = main({})
+        from webtest import TestApp
+
+        self.test_app = TestApp(app)
+
+    def test_home(self):
+        response = self.test_app.get('/', status=200)
+        self.assertIn(b'<body>Visit', response.body)
+
+    def test_hello(self):
+        response = self.test_app.get('/howdy', status=200)
+        self.assertIn(b'<body>Go back', response.body)
